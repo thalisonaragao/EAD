62e9caab3e96d40dd10fed33506944680c4b2dec
Thalison Aragão <thalissonaragao01@gmail.com>
2025-01-21 16:36:38 -0300

    Specification nos controllers

--- course/src/main/java/com/ead/course/service/CourseService.java
+++ course/src/main/java/com/ead/course/service/CourseService.java
@@ -1,17 +1,20 @@
 package com.ead.course.service;
 
 import com.ead.course.models.CourseModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CourseService {
     void delete(CourseModel courseModel);
 
-   CourseModel save(CourseModel courseModel);
+    CourseModel save(CourseModel courseModel);
 
     Optional<CourseModel> findById(UUID courseId);
 
-    List<CourseModel> findAll();
+    Page<CourseModel> findAll(Specification<CourseModel> spec, Pageable pageable);
+
 }
