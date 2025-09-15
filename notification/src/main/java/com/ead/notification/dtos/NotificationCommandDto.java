6a6b5b2bec2b5e7ee46e1039acd264ec6c9ee28b
Thalison Aragão <thalissonaragao01@gmail.com>
2025-09-15 19:17:38 -0300

    Criar Exchange Topic, definir Queue e Binding

---
+++ notification/src/main/java/com/ead/notification/dtos/NotificationCommandDto.java
@@ -0,0 +1,33 @@
+package com.ead.notification.dtos;
+
+import java.util.UUID;
+
+public class NotificationCommandDto {
+    private String tittle;
+    private String message;
+    private UUID userId;
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+}
--- notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.notification.services.impl;
 
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,10 @@ public class NotificationServiceImpl implements NotificationService {
 
     public NotificationServiceImpl(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
     }
 }
