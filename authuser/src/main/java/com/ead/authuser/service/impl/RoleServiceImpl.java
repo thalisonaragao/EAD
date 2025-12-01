eacc8b0cbecb47bdef342c558c4ba70ca662cf45
Thalison Aragão <thalissonaragao01@gmail.com>
2025-12-01 16:59:38 -0300

    Users Roles com Encoder Password em AuthUser

--- authuser/src/main/java/com/ead/authuser/service/impl/RoleServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/service/impl/RoleServiceImpl.java
@@ -1,11 +1,21 @@
 package com.ead.authuser.service.impl;
 
+import com.ead.authuser.enums.RoleType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.repositories.RoleRepository;
+import com.ead.authuser.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class RoleServiceImpl {
+public class RoleServiceImpl implements RoleService {
     @Autowired
     RoleRepository roleRepository;
+
+    @Override
+    public Optional<RoleModel> findByRoleName(RoleType roleType) {
+        return roleRepository.findByRoleName(roleType);
+    }
 }
