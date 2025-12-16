19763e0e34ebefc8ca81f316cba423a28aeb78b2
Thalison Aragão <thalissonaragao01@gmail.com>
2025-12-15 21:22:17 -0300

    Implementar JwtProvider para gerar tokens

---
+++ authuser/src/main/java/com/ead/authuser/dtos/JwtDto.java
@@ -0,0 +1,13 @@
+package com.ead.authuser.dtos;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class JwtDto {
+    @NonNull
+    private String token;
+    private String type = "Bearer";
+}
